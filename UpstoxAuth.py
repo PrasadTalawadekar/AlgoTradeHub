def9255841e3adb9b06245d16ab75810af44367f
Prasad Talawadekar <prasadtalawadekar@gmail.com>
2025-01-07 16:21:13 +0530

    All user related,charges related, margin related and order related methods being updated

--- UpstoxAuth.py
+++ UpstoxAuth.py
@@ -4,10 +4,6 @@ class Upstox_auth:
     redirect_uri_input = 'https://127.0.0.1:5000/'
     client_secret_input = '10arbpucj2'
 
-
-
-    
-
     def generate_upstox_url():
         base_url = "https://api.upstox.com/v2/login/authorization/dialog"
         params = {
@@ -71,17 +67,16 @@ class Upstox_auth:
             today_date = datetime.today().date()
 
             if last_modified_date == today_date:
-                print("File is updated")
+                
+                with open(file_path, 'r') as file:
+                    token = file.read()
             else:
                 print("File is not updated")
                 upstox_url = Upstox_auth.generate_upstox_url()
                 print("Generated URL:", upstox_url)
                 code = input("Enter the code from the redirect URL: ")
                 token = Upstox_auth.get_upstox_token(code)
-                
-
-
-                # Write code to the file
+                # Create and write to the file
                 with open(file_path, 'w') as file:
                     file.write(token)
         else:
@@ -93,9 +88,262 @@ class Upstox_auth:
             with open(file_path, 'w') as file:
                 file.write(token)
 
+        return token
+
+    #To check the data
+
+    def Profile_check(token):
+        url = 'https://api.upstox.com/v2/user/profile'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+        response = requests.get(url, headers=headers)
+
+        return (response.json())    
+
+    def Fund_margin_check(token):
+        url = 'https://api.upstox.com/v2/user/get-funds-and-margin'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+        response = requests.get(url, headers=headers)
+
+        return (response.json())
+
+        #if need to get only equity margin
+        #return (response.json()['data']['equity'])
+
+        #if need to get only commodity margin
+        #return (response.json()['data']['commodity'])
+
+    def Brokerage_check(instrument_token, quantity, product, transaction_type, price,token):
+        url = 'https://api.upstox.com/v2/charges/brokerage'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        params = {
+            'instrument_token': instrument_token,
+            'quantity': quantity,
+            'product': product,
+            'transaction_type': transaction_type,
+            'price': price
+        }
+        response = requests.get(url, headers=headers, params=params)
+
+        return response.json()
+
+    def Margin_check(instrument_key, quantity, transaction_type, product, token):
+        url = "https://api.upstox.com/v2/charges/margin"
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
+        data = {
+            "instruments": [
+                {
+                    "instrument_key": instrument_key,
+                    "quantity": quantity,
+                    "transaction_type": transaction_type,
+                    "product": product
+                }
+            ]
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+
+        print(response.status_code)
+        print(response.json())
+
+    #to do actions
+    def place_order(quantity:int, product, validity, price, tag, instrument_token, order_type, transaction_type, disclosed_quantity, trigger_price, is_amo, token):
+
+        url = 'https://api-hft.upstox.com/v2/order/place'
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}',
+        }
+
+        data = {
+            'quantity': quantity,                           #Numerical Quantity expected
+            'product': product,                             #D for Delivery &  I for Intraday
+            'validity': validity,                           #Day or IOC 
+            'price': price,                                 #Numerical Price expected
+            'tag': tag,                                     #String expected no definition
+            'instrument_token': instrument_token,           #IN value with prefix of NSE_EQ or BSE_EQ
+            'order_type': order_type,                       #MARKET, LIMIT, SL, SL-M
+            'transaction_type': transaction_type,           #BUY or SELL
+            'disclosed_quantity': disclosed_quantity,       #Numerical Quantity expected 0 for no disclosure
+            'trigger_price': trigger_price,                 #Numerical Price expected 0 for no trigger price
+            'is_amo': is_amo,                               #Boolean value expected True or False
+            #'slice': True                                  #add if needed to Slice the order
+        }
+
+        try:
+            # Send the POST request
+            response = requests.post(url, json=data, headers=headers)
+
+            # Print the response status code and body
+            print('Response Code:', response.status_code)
+            print('Response Body:', response.json())
+            return (response.json()['data']['order_id'])
+
+        except Exception as e:
+            # Handle exceptions
+            print('Error:', str(e))
+
+    def modify_order(quantity,validaty,price,order_id,order_type,disclosed_quantity,trigger_price,token):
+
+        url = 'https://api-hft.upstox.com/v2/order/modify'
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        data = {
+            'quantity': quantity,
+            'validity': validaty,
+            'price': price,
+            'order_id': order_id,
+            'order_type': order_type,
+            'disclosed_quantity': disclosed_quantity,
+            'trigger_price': trigger_price
+        }
+
+        response = requests.put(url, headers=headers, json=data)
+        print(response.text)
+
+    def get_order_details(order_id,token):
+
+        url = 'https://api.upstox.com/v2/order/details'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        params = {'order_id': order_id}
+
+        response = requests.get(url, headers=headers, params=params)
+
+        print(response.text)
+
+    def get_orderbook(token):
+        url = 'https://api.upstox.com/v2/order/retrieve-all'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+        response = requests.get(url, headers=headers)
+        print(response.text)
+
+    def get_order_history(order_id,token):
+        url = 'https://api.upstox.com/v2/order/history'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        params = {'order_id': order_id}
+
+        response = requests.get(url, headers=headers, params=params)
+
+        print(response.text)
+
+    def cancel_order(order_id,token):
+        import requests
+
+        url = f'https://api-hft.upstox.com/v2/order/cancel?order_id={order_id}'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        response = requests.delete(url, headers=headers)
+
+        print(response.text)
+
+    def exit_all_positions(token):
+        url = 'https://api.upstox.com/v2/order/positions/exit'
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}',
+        }
+
+        data = {}
+
+        try:
+            # Send the POST request
+            response = requests.post(url, json=data, headers=headers)
+
+            # Print the response status code and body
+            print('Response Code:', response.status_code)
+            print('Response Body:', response.json())
+
+        except Exception as e:
+            # Handle exceptions
+            print('Error:', str(e))
+
+    def cancel_all_orders(token):
+        url = 'https://api.upstox.com/v2/order/multi/cancel'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+        response = requests.delete(url, headers=headers)
+        print(response.text)
+
+    def get_today_trade(token):
+    
+        url = 'https://api.upstox.com/v2/order/trades/get-trades-for-day'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        response = requests.get(url, headers=headers)
+        print(response.text)
+
+    def get_old_trade(segment,start_date,end_date,page_number,page_size,token):
+        url = 'https://api.upstox.com/v2/charges/historical-trades'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+        params = {
+            'segment': segment,
+            'start_date': start_date,
+            'end_date': end_date,
+            'page_number': str(page_number),
+            'page_size': str(page_size)
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+    def get_trade_ordernum(order_id,token):
+        url = f'https://api.upstox.com/v2/order/trades?order_id={order_id}'
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        response = requests.get(url, headers=headers)
+        print(response.text)
+
+token = Upstox_auth.check_current_directory()
 
 
 
-
-
-Upstox_auth.check_current_directory()
+    
